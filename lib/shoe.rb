7dc2747ad3c2ae3938f3f32ed57b752dc2d37367
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-24 23:19:53 +0000

    automatically backed up by learn

---
+++ lib/shoe.rb
@@ -0,0 +1,13 @@
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+    def initialize(brand
+      @brand = brand
+    end
+
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+
+  end
